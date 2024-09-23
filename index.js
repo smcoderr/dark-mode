05f9d972a288f0c54eb9f727c4d24bcaae83df86
sourav-mm <souravoz2018@gmai.com>
2024-09-23 20:15:30 +0530

    second main

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let icon = document.getElementById('icon');
 icon.onclick = function(){
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
-        icon.src = '/img/sun.png';
+        icon.src = './img/sun.png';
     }else{
-        icon.src = '/img/moon.png';
+        icon.src = './img/moon.png';
     }
 }
